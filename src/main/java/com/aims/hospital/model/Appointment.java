204684b3c1b9abe6eac09c3bbbb4d8d6db29d896
JyothiPrakash1108 <jyothiprakash1108@gmail.com>
2025-03-31 11:22:45 +0530

    added appointment entity mapped for patient and doctor

--- src/main/java/com/aims/hospital/model/Appointment.java
+++ src/main/java/com/aims/hospital/model/Appointment.java
@@ -1,2 +1,64 @@
-package com.aims.hospital.model;public class Appointment {
+package com.aims.hospital.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "appointments")
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne
+    @JoinColumn(name = "patient_id",nullable = false)
+    private Patient patient;
+    @ManyToOne
+    @JoinColumn(name = "doctor_id",nullable = false)
+    private Doctor doctor;
+    private LocalDateTime localDateTime;
+    private String status;
+
+    public Appointment() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
