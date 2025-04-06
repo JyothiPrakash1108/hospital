320d04d24759627af8979e282ac4193eb9b6b4ed
JyothiPrakash1108 <jyothiprakash1108@gmail.com>
2025-04-06 19:54:00 +0530

    added

--- src/main/java/com/aims/hospital/model/DoctorAvailability.java
+++ src/main/java/com/aims/hospital/model/DoctorAvailability.java
@@ -1,4 +1,71 @@
 package com.aims.hospital.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
 public class DoctorAvailability {
+    private int id;
+    private LocalDate localDate;
+    private boolean available = true;
+    private LocalTime startTime = LocalTime.of(9,0);
+    private LocalTime endTime = LocalTime.of(17,0);
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+    public DoctorAvailability() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(LocalDate localDate) {
+        this.localDate = localDate;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
 }
